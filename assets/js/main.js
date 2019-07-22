a60c4ea5decd23ff88b7847f805e5057684c50d3
Shivam0Anand <shivam_anand@outlook.com>
2019-07-22 16:11:53 +0530

    modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,7 @@ var jsQuestons = [
     choiceB: "B: Lydia and ReferenceError",
     choiceC: "C: ReferenceError and 21",
     choiceD: "D: undefined and ReferenceError",
+    correctAnswer : choiceD,
     answer: "Answer: D - 'undefined and ReferenceError'",
     discription: "Within the function, we first declare the name variable with the var keyword. This means that the variable gets hoisted (memory space is set up during the creation phase) with the default value of undefined, until we actually get to the line where we define the variable. We haven't defined the variable yet on the line where we try to log the name variable, so it still holds the value of undefined.",
       
@@ -14,6 +15,30 @@ var jsQuestons = [
     choiceA: "A: 0 1 2 and 0 1 2",
     choiceB: "B: 0 1 2 and 3 3 3",
     choiceC: "C: 3 3 3 and 0 1 2",
+    choiceD: "None",
+    correctAnswer : choiceC,
+    answer: "Answer: C - '3 3 3 and 0 1 2'",
+    discription: "Because of the event queue in JavaScript, the setTimeout callback function is called after the loop has been executed. Since the variable i in the first loop was declared using the var keyword, this value was global. During the loop, we incremented the value of i by 1 each time, using the unary operator ++. By the time the setTimeout callback function was invoked, i was equal to 3 in the first example."
+  },
+  {
+    question:'<iframe src="https://carbon.now.sh/embed/?bg=rgba(171%2C%20184%2C%20195%2C%201)&t=seti&wt=sharp&l=javascript&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=false&pv=0px&ph=0px&ln=true&fm=Hack&fs=16.5px&lh=133%25&si=false&es=2x&wm=false&code=const%2520shape%2520%253D%2520%257B%250A%2520%2520radius%253A%252010%252C%250A%2520%2520diameter()%2520%257B%250A%2520%2520%2520%2520return%2520this.radius%2520*%25202%253B%250A%2520%2520%257D%252C%250A%2520%2520perimeter%253A%2520()%2520%253D%253E%25202%2520*%2520Math.PI%2520*%2520this.radius%250A%257D%253B%250A%250Aconsole.log(shape.diameter())%253B%250Aconsole.log(shape.perimeter())%253B" sandbox="allow-scripts allow-same-origin"></iframe>',
+    choiceA:"A: 20 and 62.83185307179586",
+    choiceB:"B: 20 and NaN",
+    choiceC:"C: 20 and 63",
+    choiceD:"D: NaN and 63",
+    correctAnswer: choiceB,
+    answer: "Answer: B - 'B: 20 and NaN'",
+    discription: "Note that the value of diameter is a regular function, whereas the value of perimeter is an arrow function. With arrow functions, the this keyword refers to its current surrounding scope, unlike regular functions! This means that when we call perimeter, it doesn't refer to the shape object, but to its surrounding scope (window for example). There is no value radius on that object, which returns undefined."
+  },
+  {
+    question: '<iframe src="https://carbon.now.sh/embed/?bg=rgba(171%2C%20184%2C%20195%2C%201)&t=seti&wt=sharp&l=javascript&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=false&pv=0px&ph=0px&ln=true&fm=Hack&fs=16.5px&lh=133%25&si=false&es=2x&wm=false&code=%252Btrue%253B%250A!%2522Lydia%2522%253B" sandbox="allow-scripts allow-same-origin"> </iframe>',
+    choiceA: "A: 1 and false",
+    choiceB: "B: false and NaN",
+    choiceC: "C: false and false",
+    choiceD: "None",
+    correctAnswer: choiceA,
+    answer: "Answer: B - false and NaN",
+    discription: "The unary plus tries to convert an operand to a number. true is 1, and false is 0. The string 'Lydia' is a truthy value. What we're actually asking, is 'is this truthy value falsy?'. This returns false."
   }
 ];
 
@@ -23,9 +48,18 @@ var choiceB = document.querySelector(".choiceB");
 var choiceC = document.querySelector(".choiceC");
 var choiceD = document.querySelector(".choiceD");
 var answer = document.getElementById("answer");
+var next = document.getElementById("next");
 var discription = document.getElementById("discription");
 
-question.innerHTML = `${jsQuestons[0].question}`;
+// Show questions 
+
+
+next.addEventListener("click", showNext);
+function showNext(e) {
+  console.log(event);
+}
+
+question.innerHTML = `${jsQuestons[3].question}`;
 choiceA.innerText = `${jsQuestons[0].choiceA}`;
 choiceB.innerText = `${jsQuestons[0].choiceB}`;
 choiceC.innerText = `${jsQuestons[0].choiceC}`;
